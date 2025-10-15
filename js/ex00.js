d693425c7d753281ec28ac872304267a56c225b6
TheoArmstrong <theo10ap@gmail.com>
2025-10-15 15:55:23 -0300

    ex00 atualização
    add multi, divi e sub

--- js/ex00.js
+++ js/ex00.js
@@ -9,6 +9,39 @@ function somar(){
         r=parseFloat(v1)+parseFloat(v2);
         document.getElementById("resultado").innerHTML=`o resultrado da soma é: ${r}`
     }
+}
 
+function multiplica(){
+    v1=document.getElementById("primeirovalor").value
+    v2=document.getElementById("segundovalor").value
+    if(v1=="" || v2==""){
+        alert("Os campos devem estra preenchidos");
+    }
+    else{
+        r=parseFloat(v1)*parseFloat(v2);
+        document.getElementById("resultado").innerHTML=`o resultrado da multiplicação é: ${r}`
+    }
+}
 
+function dividir(){
+    v1=document.getElementById("primeirovalor").value
+    v2=document.getElementById("segundovalor").value
+    if(v1=="" || v2=="" || v2==0){
+        alert("Campos invalidos");
+}else{
+    r=parseFloat(v1)/parseFloat(v2)
+    document.getElementById("resultado").innerHTML=`o resultado da divisão entre os valores é: ${r}`
+}
+}
+
+function subtrair(){
+    v1=document.getElementById("primeirovalor").value
+    v2=document.getElementById("segundovalor").value
+    if(v1=="" || v2==""){
+        alert("Os campos devem estra preenchidos");
+    }   
+    else{
+        r=parseFloat(v1)-parseFloat(v2);
+        document.getElementById("resultado").innerHTML=`o resultrado da subtração é: ${r}`
+    }
 }
